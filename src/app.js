b4aa16db0e475b04deddf2af9141f15ed63994e1
Niko Cantero <97130632+nyabinary@users.noreply.github.com>
2025-08-12 13:56:53 +0000

    generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed on", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during lunch",
+    "while I was praying",
+  ];
+
+  function randomItem(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  let randomExcuse = `${randomItem(who)} ${randomItem(action)} ${randomItem(
+    what
+  )} ${randomItem(when)}.`;
+
+  document.getElementById("excuse").innerHTML = randomExcuse;
 };
